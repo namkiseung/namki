21e2a3db5466541ec939adb48f2e951f146493b7
namkiseung <rltmd1004@naver.com>
2018-10-26 23:48:43 +0900

    google login

---
+++ refreshToken.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action="https://www.googleapis.com/oauth2/v4/token" method="post" enctype="application/x-www-form-urlencoded">
+      <input type="text" name="client_id" value="3870214908-3fur6lisge1riduu6bfncvgq15bs0k1n.apps.googleusercontent.com"><br>
+      <input type="text" name="client_secret" value="teUYcbqLdUqg3UTwd90J4feK"><br>
+      <input type="text" name="refresh_token" value="1/Eatu7T-ge3I1ekgK3E5rv24cbZdRIH84H00W14MYZbY"><br>
+      <input type="text" name="grant_type" value="refresh_token"><br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
